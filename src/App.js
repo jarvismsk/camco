c776b4a30c81b81adddeeaa7b301b50644d3671f
Irfan Khan <jarvismsk@gmail.com>
2023-05-23 09:32:28 +0530

    edited components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import AddressDetails from './components/AddressDetails';
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
-      <AddressDetails />
+      <Header />
     </div>
   );
 };
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const topbar = () => {
+    return(
+       <header>
+        <h1>My Header</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+       </header>
+    );
+};
+
+export default topbar;
